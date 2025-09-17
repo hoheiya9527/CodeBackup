1d2ddafac9351bbc916a05de2813537cbb1b5fed
DESKTOP-LIARFE3\HOHEIYA <hoheiya@hotmail.com>
2025-09-18 00:04:36 +0800

    fix dlna

--- Tvbox-Port/app/src/main/java/com/github/tvbox/osc/ui/dialog/CastListDialog.java
+++ Tvbox-Port/app/src/main/java/com/github/tvbox/osc/ui/dialog/CastListDialog.java
@@ -1,6 +1,7 @@
 package com.github.tvbox.osc.ui.dialog;
 
 import android.content.Context;
+import android.service.controls.DeviceTypes;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -71,6 +72,8 @@ public class CastListDialog extends CenterPopupView {
     @Override
     protected void onCreate() {
         super.onCreate();
+        Log.d("hoheiya", "CastListDialog onCreate()");
+
         DLNACastManager.INSTANCE.bindCastService(App.getInstance());
         findViewById(R.id.btn_cancel).setOnClickListener(view -> {
             dismiss();
@@ -93,60 +96,68 @@ public class CastListDialog extends CenterPopupView {
             if (item != null) {
                 cast(item, castVideo);
 //                DLNACastManager.INSTANCE.cast(item, castVideo);
-                dismiss();
-                if (dlnaCallback != null) {
-                    dlnaCallback.onDLNA();
-                }
+//                dismiss();
+//                if (dlnaCallback != null) {
+//                    dlnaCallback.onDLNA();
+//                }
             }
         });
     }
 
     private void cast(Device item, CastVideo castVideo) {
+        Log.d("hoheiya", "connectDevice " + item.getDetails().getFriendlyName());
         deviceControl = DLNACastManager.INSTANCE.connectDevice(item,
                 new OnDeviceControlListener() {
                     @Override
                     public void onConnected(@NonNull Device<?, ?, ?> device) {
-                        if (deviceControl == null) {
-                            return;
-                        }
-                        ToastUtils.showShort("设备[" + item.getDetails().getFriendlyName() + "] 连接成功");
+                        Log.d("hoheiya", "设备[" + item.getDetails().getFriendlyName() + "] 连接成功");
                         deviceControl.setAVTransportURI(castVideo.getUri(), castVideo.getName(),
                                 new ServiceActionCallback<Unit>() {
                                     @Override
                                     public void onSuccess(Unit unit) {
-                                        ToastUtils.showShort("投放成功");
+                                        DLNACastManager.INSTANCE.disconnectDevice(device);
+                                        if (dlnaCallback != null) {
+                                            dlnaCallback.onDLNA();
+                                        }
+                                        dismiss();
                                     }
 
                                     @Override
                                     public void onFailure(@NonNull String s) {
-                                        ToastUtils.showShort("投放失败，" + s);
+                                        DLNACastManager.INSTANCE.disconnectDevice(device);
+                                        ToastUtils.showShort("投屏失败，" + s);
+                                        if (dlnaCallback != null) {
+                                            dlnaCallback.onClose();
+                                        }
+                                        dismiss();
                                     }
                                 });
                     }
 
                     @Override
                     public void onDisconnected(@NonNull Device<?, ?, ?> device) {
-                        ToastUtils.showShort("设备[" + item.getDetails().getFriendlyName() + "] 连接已断开");
+                        Log.d("hoheiya", "设备[" + item.getDetails().getFriendlyName() + "] 连接已断开");
                     }
 
                     @Override
                     public void onEventChanged(@NonNull EventedValue<?> eventedValue) {
+                        Log.d("hoheiya", "onEventChanged " + eventedValue);
 
                     }
 
                     @Override
                     public void onAvTransportStateChanged(@NonNull TransportState transportState) {
-
+                        Log.d("hoheiya", "onAvTransportStateChanged " + transportState);
                     }
 
                     @Override
                     public void onRendererVolumeChanged(int i) {
-
+                        Log.d("hoheiya", "onRendererVolumeChanged " + i);
                     }
 
                     @Override
                     public void onRendererVolumeMuteChanged(boolean b) {
-
+                        Log.d("hoheiya", "onRendererVolumeMuteChanged " + b);
                     }
                 });
 
