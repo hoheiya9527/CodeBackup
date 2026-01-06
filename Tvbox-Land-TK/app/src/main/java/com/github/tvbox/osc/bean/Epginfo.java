498a3439aa4d344a78d0d72c03deebd170b34552
DESKTOP-LIARFE3\HOHEIYA <hoheiya@hotmail.com>
2026-01-07 02:14:24 +0800

    修正epg解析导致的崩溃

--- Tvbox-Land-TK/app/src/main/java/com/github/tvbox/osc/bean/Epginfo.java
+++ Tvbox-Land-TK/app/src/main/java/com/github/tvbox/osc/bean/Epginfo.java
@@ -1,5 +1,7 @@
 package com.github.tvbox.osc.bean;
 
+import android.text.TextUtils;
+
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,8 +16,8 @@ public class Epginfo {
     public String title;
     public String originStart;
     public String originEnd;
-    public String start;
-    public String end;
+    public String start = "";
+    public String end = "";
     public int index;
     public Date epgDate;
     public String currentEpgDate = null;
@@ -28,16 +30,24 @@ public class Epginfo {
         originStart = str1;
         originEnd = str2;
         index = pos;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
-        SimpleDateFormat userSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
-        userSimpleDateFormat.setTimeZone(TimeZone.getDefault());
-        startdateTime = userSimpleDateFormat.parse(simpleDateFormat.format(date) + " " + str1 + ":00 GMT+8:00", new ParsePosition(0));
-        enddateTime = userSimpleDateFormat.parse(simpleDateFormat.format(date) + " " + str2 + ":00 GMT+8:00", new ParsePosition(0));
-        SimpleDateFormat zoneFormat = new SimpleDateFormat("HH:mm");
-        start = zoneFormat.format(startdateTime);
-        end = zoneFormat.format(enddateTime);
-        datestart = Integer.parseInt(start.replace(":", ""));
-        dateend = Integer.parseInt(end.replace(":", ""));
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+            SimpleDateFormat userSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+            userSimpleDateFormat.setTimeZone(TimeZone.getDefault());
+            String timezone = " GMT+8:00";
+            if (!TextUtils.isEmpty(str1) && str1.length() <= 5) {
+                timezone = ":00" + timezone;
+            }
+            startdateTime = userSimpleDateFormat.parse(simpleDateFormat.format(date) + " " + str1 + timezone, new ParsePosition(0));
+            enddateTime = userSimpleDateFormat.parse(simpleDateFormat.format(date) + " " + str2 + timezone, new ParsePosition(0));
+            SimpleDateFormat zoneFormat = new SimpleDateFormat("HH:mm");
+            start = zoneFormat.format(startdateTime);
+            end = zoneFormat.format(enddateTime);
+            datestart = Integer.parseInt(start.replace(":", ""));
+            dateend = Integer.parseInt(end.replace(":", ""));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
